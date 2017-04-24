a46408002ec7d864e03ef016a27f3b091f9abe0a
MadamoiselleA <anastasia.lomakina26@gmail.com>
2017-04-24 08:44:18 -0400

    Add files via upload

---
+++ meanie.java
@@ -0,0 +1,43 @@
+
+public abstract class meanie {
+
+	public meanie(String [][]location)
+	{
+		
+	}//need to set weapon, set health, get location
+	
+	protected int healthM=4;
+	protected String[][] locationM=new String [8][8];
+	
+	public int getHealthM ()
+	{
+		return healthM;
+	}//get the meanie health
+	
+	public void setHealthM (int healthM)
+	{
+		this.healthM=healthM;
+	}//setting health of meanie
+	
+	public void hitM(String weaponP)
+	{
+		if (weaponP.equals("sword"))//if the item the player has is a basic sword, he gets .5 heats off
+		{
+		healthM=healthM-1;	
+		}
+		else if (weaponP.equals(""))
+		{
+			
+		}
+	}
+	
+	public String[][] getLocationM ()
+	{
+		return locationM;
+	}//returning location of meanie
+	
+	public void setLocationM (int xM, int yM)
+	{
+		this.locationM[xM][yM]=locationM [xM][yM];
+	}//setting location of meanie
+}
